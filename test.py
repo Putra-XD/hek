f09b4200117cf36a2204f2f2bcb4bd168bc6e756
Putra XD <97390984+Putra-XD@users.noreply.github.com>
2022-04-27 16:12:29 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,130 @@
+import os
+import time
+from time import sleep
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel as nel
+from rich import print as cetak
+os.system('clear')
+def ___banner___putra___ganteng():
+     print(f"""
+
+ ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀
+▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ 
+▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ 
+░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ 
+░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄
+ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒
+ ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░
+ ░  ░░ ░  ░   ▒   ░        ░ ░░ ░ 
+ ░  ░  ░      ░  ░░ ░      ░  ░   
+                  ░               
+         \033[44m\033[1;37m Author  \033[41m\033[1;37m Putra-XD \x1b[0m  """)
+___banner___putra___ganteng()
+table = Table(title="Tools Share Facebook")
+table.add_column(">>>>>>>", style="cyan", no_wrap=True)
+table.add_column("Informasi Pengembang/Author", style="blue")
+table.add_column("<<<", justify="right", style="blue")
+
+table = Table(title="Selamat Datang", style="blue")
+
+table.add_column("NO", style="cyan", no_wrap=True)
+table.add_column("       Menu", style="green")
+table.add_column("Status", justify="right", style="green")
+
+table.add_row("1", "Geser bulan 🌒", "ON")
+table.add_row("2", "Geser matahari ☀️", "ON")
+table.add_row("3", "Geser satelit nasa 🚀", "ON")
+table.add_row("4", "Geser bumi 🌏", "ON")
+table.add_row("5", "Doa menghapus dosa 🙏", "ON")
+
+console = Console()
+console.print(table)
+option=int(input('[=] pilih opsi : '))
+if option== 1:
+        print(' |__proses mengeser 🌒')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🌒')
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🌒')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🌒')
+        sleep(0.9)
+        print(' |__proses mengeser 🌒')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' \__berhasil menggeser bulan 🌒 >_<')
+if option== 2:
+        print(' |__proses mengeser ☀️')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser ☀️')
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser ☀️')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser ☀️')
+        sleep(0.9)
+        print(' |__proses mengeser ☀️')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' \__berhasil menggeser matahari ☀️ >_<')
+if option== 3:
+        print(' |__proses mengeser 🚀')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🚀')
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🚀')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🚀')
+        sleep(0.9)
+        print(' |__proses mengeser 🚀')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' \__berhasil menggeser satelit 🚀 >_<')
+if option== 4:
+        print(' |__proses mengeser 🌏')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🌏')
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🌏')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' |__proses mengeser 🌏')
+        sleep(0.9)
+        print(' |__proses mengeser 🌏')
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        sleep(0.9)
+        print(' \__berhasil menggeser bumi 🌏 >_<')
+if option== 5:
+	print('\__walaupun kalian crack terus setidaknya kalian berdoa')
+	print('\__agar di ampuni dosanya')
+	print('\__ingat umur nga ada yang tau 😁')
+	print('\__kamu akan di arahkan ke web!')
+	import os
+	os.system('xdg-open https://images.app.goo.gl/DvPQsrNVLDPVL5qC7')
